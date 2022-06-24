1f4d62ae76dfb0b03e8acc662d44410cb1a94587
Alabi Sunday <alabisundayayofe@gmail.com>
2022-06-24 02:10:03 -0700

    First time pushing basic aritmetic calculator

---
+++ script.js
@@ -0,0 +1,40 @@
+//Select arithmetic operation
+let AritOperator = prompt("Please enter the arithmetic operator");
+
+let userValue1 = parseInt(prompt("Enter a Value"));
+//Getting the firstvalue from the user
+
+let userValue2 = parseInt(prompt("Enter a Value"));
+//Getting the secondvalue from the user
+
+if (AritOperator == "+") {
+  let answer = userValue1 + userValue2;
+  console.log(
+    "Adittion of " + userValue1 + " + " + userValue2 + " = " + answer
+  );
+}
+//Test if AritOperator is equal to "+", then sum up the firstvalue and the secondvalue user input
+
+if (AritOperator == "-") {
+  let answer = userValue1 - userValue2;
+  console.log(
+    "Substraction of " + userValue1 + " - " + userValue2 + " = " + answer
+  );
+}
+//Test if AritOperator is equal to "-", then substract the secondvalue from the firstnumber
+
+if (AritOperator == "*") {
+  let answer = userValue1 * userValue2;
+  console.log(
+    "Multiplication of " + userValue1 + " * " + userValue2 + " = " + answer
+  );
+}
+//Test if AritOperator is equal to "*", then multiply the firstvalue by the secondvalue
+
+if (AritOperator == "/") {
+  let answer = userValue1 / userValue2;
+  console.log(
+    "Division of " + userValue1 + " / " + userValue2 + " = " + answer
+  );
+}
+//Test if AritOperator is equal to "/", then the divide the two numbers
